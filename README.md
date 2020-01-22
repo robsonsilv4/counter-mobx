b772ffd579378c92ce5b220ddbebd361d00f220c
Robson <robson@localhost.localdomain>
2020-01-22 13:33:01 -0300

    Projeto inicial

---
+++ README.md
@@ -0,0 +1,3 @@
+# Contador com MobX
+
+Projeto para estudos sobre gerenciamento de estados com Flutter e MobX.
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+// import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Contador com MobX'),
+      ),
+    );
+  }
+}
